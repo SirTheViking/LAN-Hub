9a8e248cf82d8a46fccedc88c705d4fabe4d5935
Poly <alexhexan22@gmail.com>
2019-02-02 19:10:38 +0100

    Flesh out the login page

--- website/index.php
+++ website/index.php
@@ -11,14 +11,18 @@
         
         <title>Welcome</title>
 
-        <link type="text/css" rel="stylesheet" href="styles/login.css">
-        
-        <!-- Poppins font from google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Poppins:400,700" rel="stylesheet"> 
+        <link type="text/css" rel="stylesheet" href="styles/front.css">
     </head>
     <body>
 
         <header> <?php echo $obj->header; ?> </header>
+        
+        <form action="/login.php" method="post">
+            <input type="text" placeholder="Username">
+            <input type="password" placeholder="Password">
+
+            <button type="submit">Enter</button>
+        </form>
 
     </body>
 </html>
