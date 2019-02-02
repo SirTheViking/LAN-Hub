36f62bbfbc0dfaab72b754a76155a2f4cfdc897f
Poly <alexhexan22@gmail.com>
2019-02-02 21:21:53 +0100

    Rethink of idea

--- website/index.php
+++ website/index.php
@@ -21,12 +21,57 @@
             <div> <?php echo $title; ?> </div>
         </header>
         
-        <form action="/login.php" method="post">
-            <input type="text" placeholder="Username">
-            <input type="password" placeholder="Password">
+        <main>
+            <div class="avatar">
+                <div class="thumb">
+                    <img src="https://wallhalla.com/thumbs/preview/e/EbnAPMvHLp.jpg">  
+                    
+                    <div class="overlay">
+                        <div class="username">
+                            Lirik
+                        </div>
+                        <div class="status">
+                            Offline
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-            <button type="submit">Enter</button>
-        </form>
+            <div class="avatar">
+                <div class="thumb">
+                    <img src="https://wallhalla.com/thumbs/preview/y/yWbQgkbI1N8.jpg">
+                    
+                    <div class="overlay">
+                        <div class="username">
+                            Waffle
+                        </div>
+                        <div class="status">
+                            Offline
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="avatar">
+                <div class="thumb current">
+                    <img src="https://wallhalla.com/thumbs/preview/k/KEEoeYv8h5Xx.jpg">
+
+                    <div class="overlay">
+                        <div class="username">
+                            Shorty
+                        </div>
+                        <div class="status">
+                            Last Login
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </main>
+
+        <footer>
+            <button>New</button>
+        </footer>
 
     </body>
 </html>
