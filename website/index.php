b71437bee8869f0744a099b0f2ecb363f62fe138
Poly <alexhexan22@gmail.com>
2019-02-03 11:26:50 +0100

    Improve icons look

--- website/index.php
+++ website/index.php
@@ -22,6 +22,7 @@
         </header>
         
         <main>
+
             <div class="avatar">
                 <div class="thumb">
                     <img src="https://wallhalla.com/thumbs/preview/e/EbnAPMvHLp.jpg">  
@@ -61,17 +62,28 @@
                             Shorty
                         </div>
                         <div class="status">
-                            Last Login
+                            Logged in
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="avatar create_new">
+                <div class="thumb">
+                      
+                    
+                    <div class="overlay">
+                        <div class="username">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>
+                        </div>
+                        <div class="status">
+                            Create
                         </div>
                     </div>
                 </div>
             </div>
             
         </main>
-
-        <footer>
-            <button>New</button>
-        </footer>
 
     </body>
 </html>
