82d5cccb2c8abe40bae52c3b86326290c284dd5f
Poly <alexhexan22@gmail.com>
2019-02-04 18:33:55 +0100

    Design improvements

--- website/index.php
+++ website/index.php
@@ -17,87 +17,98 @@
         <script src="scripts/jquery.js"></script>
     </head>
     <body>
-
-        <header> 
-            <div> <?php echo $title; ?> </div>
-        </header>
         
-        <main>
+        <div id="container">
+            <main>
 
-            <div class="avatar">
-                <div class="thumb">
-                    <img src="https://wallhalla.com/thumbs/preview/e/EbnAPMvHLp.jpg">  
-                    
-                    <div class="overlay">
-                        <div class="username">
-                            Lirik
-                        </div>
-                        <div class="status">
-                            Offline
+                <div id="1" class="avatar user">
+                    <div class="thumb current">
+                        <img src="https://wallhalla.com/thumbs/preview/e/EbnAPMvHLp.jpg">  
+                        
+                        <div class="overlay">
+                            <div class="username">
+                                Lirik
+                            </div>
+                            <div class="status">
+                                Offline
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="avatar">
-                <div class="thumb">
-                    <img src="https://wallhalla.com/thumbs/preview/y/yWbQgkbI1N8.jpg">
-                    
-                    <div class="overlay">
-                        <div class="username">
-                            Waffle
-                        </div>
-                        <div class="status">
-                            Offline
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="avatar">
-                <div class="thumb current">
-                    <img src="https://wallhalla.com/thumbs/preview/k/KEEoeYv8h5Xx.jpg">
-
-                    <div class="overlay">
-                        <div class="username">
-                            Shorty
-                        </div>
-                        <div class="status">
-                            Logged in
+                <div id="2" class="avatar user">
+                    <div class="thumb">
+                        <img src="https://wallhalla.com/thumbs/preview/y/yWbQgkbI1N8.jpg">
+                        
+                        <div class="overlay">
+                            <div class="username">
+                                Waffle
+                            </div>
+                            <div class="status">
+                                Offline
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="avatar create_new">
-                <div class="thumb">
-                      
-                    
-                    <div class="overlay">
-                        <div class="username">
-                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>
-                        </div>
-                        <div class="status">
-                            Create
+                <div id="3" class="avatar user">
+                    <div class="thumb">
+                        <img src="https://wallhalla.com/thumbs/preview/k/KEEoeYv8h5Xx.jpg">
+
+                        <div class="overlay">
+                            <div class="username">
+                                Shorty
+                            </div>
+                            <div class="status">
+                                Logged in
+                            </div>
                         </div>
                     </div>
                 </div>
+
+                <div id="4" class="avatar create_new">
+                    <div class="thumb">
+                        
+                        
+                        <div class="overlay">
+                            <div class="username">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>
+                            </div>
+                            <div class="status">
+                                Create
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </main>
+
+            <div class="modal register">
+                <form method="post" action="#">
+                    <header>Create new User</header>
+                    <input name="username" id="username" type="text" placeholder="Username">
+                    <input name="password" id="password" type="password" placeholder="Password">
+                    <button type="submit">Confirm</button>
+                </form>
+
+                <div class="cancel">Cancel</div>
             </div>
-            
-        </main>
 
-        <div class="modal">
-            <form method="post" action="#">
-                <header>Create new User</header>
-                <input id="username" type="text" placeholder="Username">
-                <input id="password" type="password" placeholder="Password">
-                <button type="submit">Confirm</button>
-            </form>
+            <div class="modal login">
+                <form method="post" action="#">
+                    <header>Password for user</header>
+                    <input id="login_password" type="password" placeholder="Password">
+                    <button type="submit">Enter</button>
+                </form>
 
-            <div class="cancel">Cancel</div>
+                <div class="cancel">Cancel</div>
+            </div>
+
+
+            <footer>
+            Please pick a user <span class="cursor"></span>
+            </footer>
         </div>
-
-
 
         <script src="scripts/main.js"></script>
     </body>
--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,18 +1,18 @@
 // Show the new user form
 $(".create_new").on("click", function() {
-    $(".modal").css({
+    $(".register").css({
         "display": "flex"
     });
 
-    $(".modal form input").each(function(i) {
+    $(".register form input").each(function(i) {
         $(this).removeClass("required");
         $(this).val("");
     });
 });
 
 // Hide the new user form
-$(".modal .cancel").on("click", function() {
-    $(".modal").css({
+$(".register .cancel").on("click", function() {
+    $(".register").css({
         "display": "none"
     });
 });
@@ -20,21 +20,86 @@ $(".modal .cancel").on("click", function() {
 
 
 // User Creation form
-$(".modal form").on("submit", function(e) {
+$(".register form").on("submit", function(e) {
     e.preventDefault();
 
-    let uname = $(".modal form #username").val();
-    let passwd = $(".modal form #password").val();
+    let uname = $(".register form #username").val();
+    let passwd = $(".register form #password").val();
 
-    // So there's no empty sends
+    // So there are no empty sends
     if(uname.length > 0 && passwd.length > 0) {
-        $.post("http://localhost:6000", {username: uname, password: passwd});
+        $.post("http://localhost:1337/register", {username: uname, password: passwd})
+            .done(function() { // Reload the page when it's done so the user list is updated
+                // TODO: Maybe login directly instead?!
+                location.reload();
+            });
+    
+    } else {
+        // Show that the fields are required
+        $(".register form input").each(function(i) {
+            $(this).addClass("required");
+        });
+    }
+});
 
-        return;
+
+
+
+
+
+/// Just goofing around
+let id = 1;
+let original = 0;
+$(".user").each(function() {
+    original++;
+});
+
+
+$(document).keyup(function(e) {
+    let key = e.which;
+
+    switch(key) {
+        case 39: // Right
+            if(id == original) {
+                break;
+            } else {
+                $("#" + id + " .thumb").removeClass("current");
+                id++;
+                $("#" + id + " .thumb").addClass("current");
+            }
+            break;
+        case 37: // Left
+            if(id == 1) {
+                break;
+            } else {
+                $("#" + id + " .thumb").removeClass("current");
+                id--;
+                $("#" + id + " .thumb").addClass("current");
+            }
+            break;
+    }
+});
+
+
+
+
+
+
+
+/// The blinker animation
+let state = true; // True visible, false invisible
+setInterval(function() {
+    let cursor = $("footer .cursor");
+
+    if(state == true) {
+        cursor.css({
+            "visibility": "hidden"
+        });
+    } else {
+        cursor.css({
+            "visibility": "visible"
+        });
     }
 
-    // Show that the fields are required
-    $(".modal form input").each(function(i) {
-        $(this).addClass("required");
-    });
-});
+    state = !state;
+}, 600);
