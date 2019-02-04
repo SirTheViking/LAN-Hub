6919f474f74462e28e046dee35fe1860594aaa2c
Poly <alexhexan22@gmail.com>
2019-02-04 18:33:36 +0100

    Separate usages and add CORS

--- user_service/api.py
+++ user_service/api.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
 
 import os
 import mysql.connector
@@ -10,7 +11,10 @@ import mysql.connector
 
 app = Flask(__name__)
 api = Api(app)
+# To allow Cors requests everywhere
+CORS(app)
 
+# Database data config
 config = {
     "user": "root",
     "password": "root",
@@ -19,20 +23,27 @@ config = {
     "database": "ip"
 }
 
+# Connect to database
 connection = mysql.connector.connect(**config)
 cursor = connection.cursor(dictionary=True)
 
 
-
+# The registration query
 add_user = "INSERT INTO users (username, password) VALUES (%s, %s);"
 
 
-
+# Parse request arguments 
+# TODO: Change this since it's going to be deprecated
 parser = reqparse.RequestParser()
 parser.add_argument("username", type=str)
 parser.add_argument("password", type=str)
 
 
+
+
+
+# / - endpoint for getting all users
+# or for deleting users
 class User(Resource):
 
     # Get users
@@ -41,13 +52,27 @@ class User(Resource):
         cursor.execute(query)
 
         data = cursor.fetchall()
-
         
-
+        # Temporary (don't send passwords back)
         return jsonify(data)
 
+
+
+    # TODO: Ability to remove accounts after login
+    def delete(self):
+        return "A DELETE", 200
+
+
+
+
+
+
+
+# /register - endpoint for registering users
+class Register(Resource):
     
     # Create user
+    # TODO: Hash passwords
     def post(self):
         args = parser.parse_args()
 
@@ -65,18 +90,40 @@ class User(Resource):
             connection.commit()
 
             return {
-                "message": "Everything Worked (WIP message)"
+                "message": "Successfully Registered"
             }, 201
 
 
-    def put(self):
-        return "A PUT", 200
+
+
+
+
+# /deleteall - Development endpoint for clearing 
+# the database if needed
+class Delete(Resource):
 
     def delete(self):
-        return "A DELETE", 200
+        query = "TRUNCATE TABLE users"
+        cursor.execute(query)
+        connection.commit()
+
+        return {
+            "message": "Users database cleared"
+        }, 204
+
+
+
+
 
 
 api.add_resource(User, "/")
+api.add_resource(Register, "/register")
+api.add_resource(Delete, "/deleteall") # Remove later
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
