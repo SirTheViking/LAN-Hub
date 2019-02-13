4ac0b8899c9c5b46476fb4da58c76a2cbea2e26a
Poly <alexhexan22@gmail.com>
2019-02-13 17:59:55 +0100

    Add comments

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,3 +1,8 @@
+/* ####
+#######
+#######  Small tweaks based on screen size
+#######
+####*/
 $(".modal").css({
     "padding-top": window.innerHeight / 5
 });
@@ -6,6 +11,12 @@ $(".modal").css({
 
 
 
+/* ####
+#######
+#######  Identicon/Profile avatar generation
+#######  based on the hash
+#######
+####*/
 let options = {
     foreground: [78, 161, 255, 100],
     background: [24, 27, 33, 255], // #181b21
@@ -14,11 +25,12 @@ let options = {
     format: "svg"
 };
 
+// For every user that was returned from the database
 $(".form_avatar img").each(function(i) {
     let hash = $(this).attr("src");
 
-    let data = new Identicon(hash, options);
-    $(this).attr("src", "data:image/svg+xml;base64," + data);
+    let data = new Identicon(hash, options); // Create new image
+    $(this).attr("src", "data:image/svg+xml;base64," + data); // Set it as source
 });
 
 
@@ -26,19 +38,29 @@ $(".form_avatar img").each(function(i) {
 
 
 
-
+/* ####
+#######
+#######  Whenever the form is submitted.
+#######  Wether it's register or login, it's handled here
+#######
+####*/
 $("form").on("submit", function(e) {
     e.preventDefault();
 
+    // Get the password, whatever is in the .username of the .current
+    // and the login url (default values)
     let passwd = $("#login_password").val();
-    let uname = $(".current .username").text().trim();;
-    let url = "http://localhost:5000/user/login";;
+    let uname = $(".current .username").text().trim();
+    let url = "http://localhost:5000/user/login";
 
+    // If .create_new is in focus that means user wants to register
     if($(".create_new").hasClass("current")) {
+        // Change username and url to new values
         uname = $("#register_username").val();
         url = "http://localhost:5000/user/register";
     }
 
+    // If password input is empty that's no good
     if(passwd.length == 0) {
         // Show that the fields are required
         $("form input").each(function(i) {
@@ -48,11 +70,10 @@ $("form").on("submit", function(e) {
         return;
     }
 
+    // Send the request to wherever it's supposed to go
     $.post(url, {username: uname, password: passwd})
-        .done(function(data) { // Reload the page when it's done so the user list is updated
-                // TODO: Maybe login directly instead?!
-                //location.reload();
-            console.log(data);
+        .done(function(data) {
+                // TODO: Redirect or something
         });
 });
 
@@ -61,43 +82,45 @@ $("form").on("submit", function(e) {
 
 
 
-
-
-
-
-
-/* MOVE LATER THIS IS FOR TESTING */
-
 /**
  * TODO ]============
  * 4. Add swipe gestures for touch devices
  * 5. Clean this code and the CSS
  */
 
-var center = window.innerWidth / 2;
-// the total width of an avatar
-var a_width = $(".current").first().outerWidth(true);
-// Just to make sure that spamming doesnt ruin it
-var margin = 0;
+
+
+
+
+ /* ####
+#######
+#######  Handling of how the "carousel" of users behaves
+#######
+####*/
+var center = window.innerWidth / 2; // Center of window
+var a_width = $(".current").first().outerWidth(true); // Total width of avatar (+margin, padding, border)
+var margin = 0; // Keep check of how much margin should actually be added
 
 
 // If the amount of users is even, center one of them
 if($(".form_avatar").length % 2 == 0) {
     margin = a_width;
     $(".avatar_container").css("margin-right", margin);
-
 }
 
+// When document is ready, find the icon in the center
+// and give it the .current class so that it stands out
 $(document).ready(function() {
     $(".form_avatar").each(function(i) {
 
         let pos = $(this).offset();
 
-        // Weird check for android chrome
+        // Weird check for android chrome (check if it's near the center)
         if(pos.left <= center + 100 && pos.left >= center - 100) {
             $(".current").removeClass("current");
             $(this).addClass("current");
 
+            // If it's the new user tile show the username input aswell
             if($(this).hasClass("create_new")) {
                 $("#register_username").css({
                     "opacity": "1",
@@ -109,7 +132,9 @@ $(document).ready(function() {
 });
 
 
-
+// Check for Key Up events in order
+// to add or remove margin from the container
+// and to change what the .current tile is
 $(document).on("keyup", function(e) {
 
     let key = e.which;
@@ -118,8 +143,10 @@ $(document).on("keyup", function(e) {
 
     switch(key) {
         case 37: //Left
+
             let prev = current.prev();
 
+            // If there's no other elements before this one
             if(prev.length == 0) {
                 return;
             }
@@ -130,10 +157,12 @@ $(document).on("keyup", function(e) {
             prev.addClass("current");
             container.css("margin-right", margin);
             break;
+
         case 39: //Right
-            // TODO: Add last and first checks
+
             let next = current.next();
 
+            // If there's no other elements after this one
             if(next.length == 0) {
                 return;
             }
