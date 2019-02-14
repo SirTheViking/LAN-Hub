15b8aa6db315bc8182c4772bf457b0c8e63c7e75
Poly <alexhexan22@gmail.com>
2019-02-14 17:33:58 +0100

    Move one-time use services into a separate folder

--- website/user/services/register.php
+++ website/user/services/register.php
@@ -1,13 +1,12 @@
 <?php
 
 // Provides database connection and response() function
-require ("db.php");
+require ("../db.php");
 
 /**
  * -[ Used status codes and what they mean:
  *      * 400 - Bad Request
- *      * 404 - Not Found
- *      * 202 - Accepted
+ *      * 201 - Created
  */
 
 
@@ -19,27 +18,20 @@ if(empty($_POST["username"]) || empty($_POST["password"])) {
 
 
 
+
 $username = htmlspecialchars($_POST["username"]);
 $password = htmlspecialchars($_POST["password"]);
+$picture_hash = hash("sha256", $username . $password . rand(1, 500));
+
+$pass_hash = password_hash($password, PASSWORD_BCRYPT);
 
 // TODO: Make sure this actually succeeds. If not handle it
-$stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username");
+$stmt = $pdo->prepare("INSERT INTO users (username, password, profile_image) VALUES (:username, :password, :profile_image);");
 $stmt->bindParam(":username", $username);
+$stmt->bindParam(":password", $pass_hash);
+$stmt->bindParam(":profile_image", $picture_hash);
 $stmt->execute();
 
-$data = $stmt->fetch();
 
-// Invalid username (somehow). This shouldn't normally happen
-if(empty($data)) {
-    respond("Wrong username. What did you do!?!", 404);
-}
-
-
-// Invalid password
-if(!password_verify($password, $data["password"])) {
-    respond("Password doesn't work with that username.", 400);
-}
-
-
-$_SESSION["logged_in"] = true;
-respond("Login successful. Welcome!", 202);
+$_SESSION["logged_in"] = true; // Will do for now
+respond("Registration succesfull.", 201);
