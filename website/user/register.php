2b86ad68d68488d4d71dc2f7f69f8fafba1b33fd
Poly <alexhexan22@gmail.com>
2019-02-14 11:02:58 +0100

    Properly implement register responses

--- website/user/register.php
+++ website/user/register.php
@@ -2,9 +2,20 @@
 
 require ("db.php");
 
+// Not used but it'll stay here for now
+$response = array(
+    "message" => "{Replace}"
+);
+
+
 // If the variables aren't set, no point in continuing
 if(empty($_POST["username"]) || empty($_POST["password"])) {
-    echo "That won't really work"; // TODO: Something more informative
+    // Bad Request
+    http_response_code(400);
+    $response["message"] = "Both fields need to be filled in!";
+
+    // Return a JSON here
+    echo json_encode($response);
     exit(0);
 }
 
@@ -18,11 +29,21 @@ $picture_hash = hash("sha256", $username . $password . rand(1, 500));
 
 $pass_hash = password_hash($password, PASSWORD_BCRYPT);
 
+// TODO: Make sure this actually succeeds. If not handle it
 $stmt = $pdo->prepare("INSERT INTO users (username, password, profile_image) VALUES (:username, :password, :profile_image);");
 $stmt->bindParam(":username", $username);
 $stmt->bindParam(":password", $pass_hash);
 $stmt->bindParam(":profile_image", $picture_hash);
 $stmt->execute();
-    
-echo "Successfully Registered : " . " Pic Hash: " . $picture_hash; // TODO: Register
+
+
+$_SESSION["logged_in"] = true; // Will do for now
+
+
+// Created
+http_response_code(201);
+$response["message"] = "Registration successfull";
+
+// Return a JSON here
+echo json_encode($response);
 
