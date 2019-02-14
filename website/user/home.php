7ab2f905435e88d099765d1e87bd26d26d9d283c
Poly <alexhexan22@gmail.com>
2019-02-14 19:37:14 +0100

    Begin work on homepage

---
+++ website/user/home.php
@@ -0,0 +1,26 @@
+<?php 
+
+    require ("templates/logged_in_check.php");
+
+?>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="theme-color" content="#181b21">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        
+        <title>Home</title>
+
+        <link type="text/css" rel="stylesheet" href="styles/front.css">
+        <script type="text/javascript" src="scripts/jquery.js"></script>
+        <script type="text/javascript" src="scripts/jquery-mobile-events.js"></script>
+        <script type="text/javascript" src="scripts/identicon.js"></script>
+    </head>
+    <body>
+
+
+
+    </body>
+</html>
