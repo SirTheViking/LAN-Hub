b96d2beedd771d5a7860153862baa72fd42c151f
Poly <alexhexan22@gmail.com>
2019-02-22 16:12:24 +0100

    Move the navbar JS into it's own component file

--- website/user/templates/nav.php
+++ website/user/templates/nav.php
@@ -29,35 +29,3 @@
         <a href="#">ICON: Link 4</a>
     </div>
 </div>
-
-
-
-
-<!-- Maybe dont have this here? might be hard to find later -->
-<script type="text/javascript">
-    // Handles hamburger icon animation
-    $(".hamburger").on("click", function() {
-        $(this).toggleClass("is-active");
-        $(".sidebar, nav").toggleClass("active");
-        
-    });
-
-
-
-    // Handle rendering the sidebar data
-    // TODO: Figure out a way to not have duplicate options and other configs
-    // a php script that runs through all .js files and adds them into a single one?
-    let options = {
-        foreground: [78, 161, 255, 100],
-        background: [24, 27, 33, 255], // #181b21
-        margin: 0.2,
-        size: 420,
-        format: "svg"
-    };
-
-    let hash = $(".profile img").attr("src");
-    let data = new Identicon(hash, options); // Create new image
-    $(".profile img").attr("src", "data:image/svg+xml;base64," + data); // Set it as source
-
-
-</script>
