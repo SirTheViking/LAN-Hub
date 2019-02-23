a9d3827a07dfcaeb7b12ebbd0245641c26df0219
Poly <alexhexan22@gmail.com>
2019-02-23 10:51:36 +0100

    Make use of the new carousel object events

--- website/scripts/components/front.js
+++ website/scripts/components/front.js
@@ -17,10 +17,10 @@ $(".modal").css({
 ####*/
 
 // For every user that was returned from the database
-$(".form_avatar img").each(function(i) {
-    let hash = $(this).attr("src");
+$(".image_hash").each(function(i) {
+    let hash = $(this).text();
     let data = new Identicon(hash, identicon_options); // Create new image
-    $(this).attr("src", "data:image/svg+xml;base64," + data); // Set it as source
+    $(this).parent().find("img").attr("src", "data:image/svg+xml;base64," + data); // Set it as source
 });
 
 
@@ -93,166 +93,29 @@ $("form").on("submit", function(e) {
 
 
 
-/* ####
-#######
-#######  Handling of how the "carousel" of users behaves
-#######
-####*/
-let center = window.innerWidth / 2; // Center of window
-let a_width = $(".current").first().outerWidth(true); // Total width of avatar (+margin, padding, border)
-let margin = 0; // Keep check of how much margin should actually be added
 
 
-// If the amount of users is even, center one of them
-if($(".form_avatar").length % 2 == 0) {
-    margin = a_width;
-    $(".avatar_container").css("margin-right", margin);
-}
-
-// When document is ready, find the icon in the center
-// and give it the .current class so that it stands out
-$(document).ready(function() {
-    $(".form_avatar").each(function(i) {
-
-        let pos = $(this).offset();
-
-        // Weird check for android chrome (check if it's near the center)
-        if(pos.left <= center + 100 && pos.left >= center - 100) {
-            $(".current").removeClass("current");
-            $(this).addClass("current");
-
-            // If it's the new user tile show the username input aswell
-            if($(this).hasClass("create_new")) {
-                $("#form_username").css({
-                    "opacity": "1",
-                    "margin-top": "0"
-                });
-            }
-        }
-    });
-});
-
-
-
-
-
-
-
-/* ####
-#######
-#######  Different events for different ways
-#######  of controlling the carousel
-#######
-####*/
-// Keyboard
-$(document).on("keyup", function(e) { 
-    let key = e.which;
-    carouselHandler(key);
-});
-// Swipes
-$(".avatar_container").on("swipe", function(e, swipe) {
-    let direction = swipe.direction;
+// Add event listener for carousel
+let carousel_container = document.getElementById("carousel_container");
+let carousel_handler = function(e) {
+    // TODO: Probably not the best way to handle this?
+    if(!document.getElementById("login")) 
+        this.removeEventListener("carousel-move", carousel_handler);
     
-    if(direction == "left") {
-        direction = "right";
-    } else {
-        direction = "left";
-    }
+    let has_class = $(".current").hasClass("create_new");
+    if(has_class)
+        $("#form_username").removeClass("hidden");
+    else
+        $("#form_username").addClass("hidden");
+            
+};
 
-    carouselHandler(direction);
-});
-// Clicks
-$(".form_avatar").on("click", function() {
-    let pos = $(this).offset();
-
-    if(pos.left < center) {
-        carouselHandler("left");
-    } else {
-        carouselHandler("right");
-    }
-});
+if(carousel_container)
+    carousel_container.addEventListener("carousel-move", carousel_handler);
 
 
 
-
-
-
-
-/* ####
-#######
-#######  Function definitions
-#######
-####*/
-
-/**
- * Move the carousel in the direction that the event
- * requests. Events may be swipes, clicks, or key presses.
- * Handle the direction, add or subtract the required margin
- * and move the .current class to the avatar in center.
- * 
- * If avatar in center is the new user one, display the username
- * input.
- * 
- * @param {string/number} key The direction in which the carousel should be going 
- */
-function carouselHandler(key) {
-    let container = $(".avatar_container");
-    let current = $(".current");
-
-    switch(key) {
-        case "left":
-        case 37: //Left
-
-            let prev = current.prev();
-
-            // If there's no other elements before this one
-            if(prev.length == 0) {
-                return;
-            }
-
-            margin -= a_width * 2;
-
-            current.removeClass("current");
-            prev.addClass("current");
-            container.css("margin-right", margin);
-            break;
-
-        case "right":
-        case 39: //Right
-
-            let next = current.next();
-
-            // If there's no other elements after this one
-            if(next.length == 0) {
-                return;
-            }
-
-            margin += a_width * 2;
-
-            current.removeClass("current");
-            next.addClass("current");
-            container.css("margin-right", margin);
-            break;
-    }
-
-    // This means the registration form should be visible
-    if($(".current").hasClass("create_new")) {
-        $("#form_username").css({
-            "opacity": "1",
-            "margin-top": "0",
-            "cursor": "text"
-        });
-    } else {
-        // Gotta hide it as best as possible
-        // while still having a smooth animation
-        $("#form_username").css({
-            "opacity": "0",
-            "margin-top": "-60px",
-            "cursor": "default"
-        });
-    }
-}
-
+    
 
 /**
  * Create a new hidden form and submit it with the required
