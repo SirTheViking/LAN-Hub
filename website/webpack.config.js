e56d1d73a4493d3c25e4c2b91947242ec91a6d2d
Poly <alexhexan22@gmail.com>
2019-02-24 18:20:50 +0100

    Add some production checks (WIP) and use new plugins to copy files

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,6 +1,22 @@
-const path              = require("path");
-const webpack           = require("webpack");
-const mini_css_plugin   = require("mini-css-extract-plugin");
+const path                  = require("path");
+
+const mini_css_plugin       = require("mini-css-extract-plugin");
+const uglify_js_plugin      = require("uglifyjs-webpack-plugin");
+const copy_plugin           = require("copy-webpack-plugin");
+
+
+let config = {};
+
+config.mini_css = {
+    filename: "app.css"
+}
+
+config.copy_plugin = [
+    { from: "index.php", to: "index.php" },
+    { from: "user", to: "user", toType: "dir" },
+    { from: "resources", to: "resources", toType: "dir" },
+    { from: ".htaccess", to: ".htaccess", toType: "file"}
+];
 
 
 
@@ -8,7 +24,8 @@ module.exports = (env, argv) => {
     let prod = argv.mode === "production";
 
     return {
-        watch: true,
+        // Don't watch if building for production
+        watch: prod ? false : true,
         // Tell webpack to begin building its
         // dependency graph from this file
         entry: path.join(__dirname, "scripts", "bootstrap.js"),
@@ -36,16 +53,32 @@ module.exports = (env, argv) => {
                 }
             ]
         },
-        plugins: [
-            /*
-            new webpack.optimize.UglifyJsPlugin({
-                compress: {
-                    warnings: false
+        plugins: prod ? [ // Run when production is true
+            // Clean the javascript and make it
+            // mostly unreadable
+            new uglify_js_plugin({
+                uglifyOptions: {
+                    warnings: false,
+                    compress: {},
+                    mangle: true,
+                    output: {
+                        comments: false
+                    }
                 }
-            })*/
-            new mini_css_plugin({
-                filename: "app.css"
-            })
+            }),
+            // All sass to one css file
+            new mini_css_plugin(config.mini_css),
+            // Copy all required files to the build folder
+            // so it can be copied to whatever server needs it
+            new copy_plugin(config.copy_plugin)
+        ] : [
+            // If development
+            // Just bundle all css for now
+            new mini_css_plugin(config.mini_css),
+            new copy_plugin(config.copy_plugin)
         ]
     }
 };
+
+
+
